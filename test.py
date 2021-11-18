8c6fbba7f900b1b57e9ccb935d0c37f9f86e31c0
angelobretenovv <angel.obretenov@contrastsecurity.com>
2021-11-18 13:59:30 +0200

    Added another file for rebase

---
+++ test.py
@@ -0,0 +1,29 @@
+
+
+def test_is_equal():
+    assert 1 == 1
+
+
+def test_strings_equal():
+    assert 'what' == 'what'
+
+def test_1is_equal():
+    assert 1 == 1
+
+
+def test_1strings_equal():
+    assert 'what' == 'what'
+
+def test_2is_equal():
+    assert 1 == 1
+
+
+def test_2strings_equal():
+    assert 'what' == 'what'
+
+def test_3is_equal():
+    assert 1 == 1
+
+
+def test_s3trings_equal():
+    assert 'what' == 'what'
